92f4a4be7052a9836b5e35540081e4dbe83af4ff
Som23Git <somesh.rokz@gmail.com>
2022-04-03 21:32:54 +0530

    Updated the other placeholders like createdAt, and description

--- routes/articles.js
+++ routes/articles.js
@@ -3,8 +3,12 @@
 const express = require('express');
 const router = express.Router();        // We can use this Router function that we can use to create view and call the same like we do in server.js.
 
-router.get('/', (req,res) => {
-    res.send('In articles');
+router.get('/new', (req,res) => {
+    res.render('articles/new');
+});
+
+router.post('/', (req,res) => {
+
 })
 
 module.exports = router;
